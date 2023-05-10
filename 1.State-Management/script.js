f5855bc24252b6e95179aa99c60e61b378d78e6c
sazzad-commlink <sazzad@commlinkinfotech.com.bd>
2023-05-10 07:45:47 +0600

    Code reusable issue

--- 1.State-Management/script.js
+++ 1.State-Management/script.js
@@ -2,7 +2,12 @@ const CounterEl = document.getElementById('counter');
 const IncrementEl = document.getElementById('increment');
 const DecrementEl = document.getElementById('decrement');
 
+const Counter2El = document.getElementById('counter2');
+const Increment2El = document.getElementById('increment2');
+const Decrement2El = document.getElementById('decrement2');
+
 let count = 0;
+let count2 = 0;
 
 IncrementEl.addEventListener('click', () => {
   count++;
@@ -11,4 +16,13 @@ IncrementEl.addEventListener('click', () => {
 DecrementEl.addEventListener('click', () => {
   count--;
   CounterEl.innerText = count;
+});
+
+Increment2El.addEventListener('click', () => {
+  count2++;
+  Counter2El.innerText = count2;
+});
+Decrement2El.addEventListener('click', () => {
+  count2--;
+  Counter2El.innerText = count2;
 });
