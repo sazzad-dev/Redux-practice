b3b94fc0db953cb36ef51807e4b2b682da85680e
Sazzad-H0ssain <sazzad.bhuiyan011@gmail.com>
2023-06-11 22:44:22 +0600

    simple counter app using react

---
+++ 2.Reusability and reactive UI/counterapp/src/App.js
@@ -0,0 +1,16 @@
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div class='w-screen h-screen p-10 bg-gray-100 text-slate-700'>
+      <h1 class='max-w-md mx-auto text-center text-2xl font-bold'>
+        Simple Counter Application
+      </h1>
+      <div class='max-w-md mx-auto mt-10 space-y-5'>
+        <Counter />
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- 2.Reusability and reactive UI/script.js
+++
@@ -1,8 +0,0 @@
-<html>
-  <head>
-    <title>Reactive UI</title>
-  </head>
-  <body>
-    <div>Hello Reactive UI</div>
-  </body>
-</html>;
